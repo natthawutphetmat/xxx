0921ca2ca568745c6bcdb8387e52ff86765f370e
natthawutphetmat <167544607+natthawutphetmat@users.noreply.github.com>
2024-05-02 03:00:31 +0700

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -2,8 +2,7 @@ import { Inter } from "next/font/google";
 import "../Styles/globals.css";
 import "../Styles/app.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Script from 'next/script';
-import Head from "next/head";
+
 
 
 const inter = Inter({ subsets: ["latin"] });
@@ -16,26 +15,7 @@ export default function RootLayout({ children }) {
     
  
 
-<head>
-<Script async src="https://www.googletagmanager.com/gtag/js?id=AW-16549026809"></Script>
-<Script>
-{`
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', 'AW-16549026809');
-            `}
-</Script>
 
-<Script>
-{`
-              gtag('event', 'conversion', {'send_to': 'AW-16549026809/1mLzCLzwyasZEPm3mNM9'});
-            `}
-</Script>
-
-
-
-</head>
 <body className={inter.className}>
         {children}
 
